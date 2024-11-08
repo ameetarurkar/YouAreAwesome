1ee85342ebee6f5fd853305168fe0b7e791ec069
Ameet Arurkar <ameet@ubidesk.com>
2024-11-08 14:07:48 -0800

    Add colors and spacers

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,30 +12,51 @@ struct ContentView: View {
     @State private var messageString = ""
     
     var body: some View {
+        
+        
         VStack {
+            
+            Spacer()
+            
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
                 .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            //                .border(.orange, width: 1)
                 .padding()
+            
+            Spacer()
             
             
             HStack {
-                
+                Spacer()
                 Button("Awesome") {
                     // This is the action performed when the button is pressed
                     messageString = "You Are Awesome!"
                 }
                 .buttonStyle(.borderedProminent)
+                .font(.title2)
+                Spacer()
                 
                 Button("Great") {
                     messageString = "You Are Great!"
                 }
                 .buttonStyle(.borderedProminent)
+                .font(.title2)
+                Spacer()
+                
             }
+            //            .border(.purple, width: 5)
+            .padding()
         }
+        
     }
 }
+
 
 #Preview {
     ContentView()
